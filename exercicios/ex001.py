3c665c5e518c2110912f97e21b2b54933a28de0d
JuliandroLessa <lessajuliandro@gmail.com>
2022-01-04 21:29:11 +0000

    atualização

---
+++ exercicios/ex001.py
@@ -0,0 +1,2 @@
+nome =input ("qual seu nome?")
+print ("È um grande prazer te conhecer!", nome)
---
+++ exercicios/ex003.py
@@ -0,0 +1,9 @@
+n1 = int(input ('digite um numero:' ))
+n2 = int(input('digite outro:'))
+s = n1 + n2 
+
+#print ('A soma entre {} e {} vale {}'.format (n1, n2, s)) aprendido no curso e, video youtube, porém apartir de 2019 mudou, novo exemplo linha baixo.
+
+print (f'A soma de {n1} e {n2} é {s}')
+
+
