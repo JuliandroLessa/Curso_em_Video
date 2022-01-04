89a2f04a27693a922f57356c90e31d72b0b866db
unknown <lessajuliandro@gmail.com>
2022-01-04 16:19:09 +0000

    primeiro commit

---
+++ scripts_Phyton_Guanabara/Desafio02.py
@@ -0,0 +1,6 @@
+dia =input ("Dia:")
+mes =input ("mês:")
+ano =input ("ano:")
+
+print ("Você nasceu dia", dia, "do mês", mes, "no ano de", ano)
+
---
+++ scripts_Phyton_Guanabara/Desafio03.py
@@ -0,0 +1,4 @@
+numero1 =input ("Primeiro numero")
+numero2 =input ("segundo numero")
+
+print ('A soma é', numero1, numero2)
